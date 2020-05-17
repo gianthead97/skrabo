089db361f67a1135d61e782f829dde6626455584
Ana Marković <ana97markovic@gmail.com>
2020-05-17 00:37:10 +0000

    Revert "small changes"
    This reverts commit 4196940dd7e1c6c185bbc0e58f963fa831d31948

--- skrabo/src/app/app.module.ts
+++ skrabo/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-import { WhiteboardComponent } from './whiteboard/whiteboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
-    WhiteboardComponent
+    FormComponent
   ],
   imports: [
     BrowserModule,
