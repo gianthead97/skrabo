71caf38d7e12e68b2dce7fe5c8bef5054582dd57
andjaam <milicevica32@gmail.com>
2020-05-19 00:50:40 +0300

    rutiranje na klijentu

--- skrabo/src/app/app-routing.module.ts
+++ skrabo/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { WhiteboardComponent } from './whiteboard/whiteboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FormComponent },
+  { path: 'game', component: WhiteboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
