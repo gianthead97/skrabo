9f42684d1c785c20ded78c8a98aa863566524e55
Jovan Mirkov <jvnmirkov@gmail.com>
2020-05-20 23:24:24 +0200

    C:/Program Files/Git/

--- skrabo/src/app/whiteboard/whiteboard.component.ts
+++ skrabo/src/app/whiteboard/whiteboard.component.ts
@@ -6,61 +6,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./whiteboard.component.css']
 })
 export class WhiteboardComponent implements OnInit {
-
-
-  board: HTMLCanvasElement;
-  ctx: CanvasRenderingContext2D;
-  active: boolean = false;
-
-  constructor() {
-
-
+  
+  
+    board: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+    active = false;
+  
+    constructor() {
+  
+  
+    }
+  
+    ngOnInit(): void {
+  
+      this.board = (document.getElementById('board') as HTMLCanvasElement);
+      this.ctx = this.board.getContext('2d');
+      this.board.addEventListener('mousedown', (evt) => {
+        // console.log(evt);
+        this.startDrawing(evt);
+      });
+      this.board.addEventListener('mouseup', (evt) => {
+        // console.log(evt);
+        this.endDrawing();
+      });
+      this.board.addEventListener('mousemove', (evt) => {
+        // console.log(evt);
+        this.draw(evt);
+      });
+  
+    }
+  
+  
+    startDrawing(e: MouseEvent): void {
+  
+      this.active = true;
+      this.draw(e);
+  
+      console.log('start drawing: active:' + this.active);
+  
+    }
+  
+    endDrawing(): void {
+  
+      this.active = false;
+      this.ctx.beginPath();
+      console.log('end drawing: active:' + this.active);
+    }
+  
+    draw(e: MouseEvent): void {
+  
+      console.log('drawing...');
+  
+      if (!this.active) { return; }
+  
+      this.ctx.lineWidth = 10;
+      // this.ctx.strokeStyle = 'black';
+      this.ctx.lineCap = 'round';
+      this.ctx.lineTo(e.clientX, e.clientY);
+      this.ctx.stroke();
+      this.ctx.beginPath();
+      this.ctx.moveTo(e.clientX, e.clientY);
+  
+    }
+  
   }
-
-  ngOnInit(): void {
-
-    this.board = <HTMLCanvasElement>document.getElementById('board');
-    this.ctx = this.board.getContext('2d');
-
-    this.board.addEventListener("mousedown", this.startDrawing);
-    this.board.addEventListener("mouseup", this.endDrawing);
-    this.board.addEventListener("mousemove", this.draw);
-
-  }
-
-
-  startDrawing(e: MouseEvent): void {
-
-    this.active = true;
-    this.draw(e);
-
-    console.log("start drawing: active:" + this.active);
-
-  }
-
-  endDrawing(): void {
-    
-    this.active = false;
-    this.ctx.beginPath();
-    console.log("end drawing: active:" + this.active);
-  }
-
-  draw(e: MouseEvent): void {
-
-    console.log("drawing...");
-
-    if (!this.active) { return; }
-
-    this.ctx.lineWidth = 10;
-    //this.ctx.strokeStyle = 'black';
-    this.ctx.lineCap = "round";
-    this.ctx.lineTo(e.clientX, e.clientY);
-    this.ctx.stroke();
-    this.ctx.beginPath();
-    this.ctx.moveTo(e.clientX, e.clientY);
-
-  }
-
-
-
-}
+  
