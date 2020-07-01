cad3071ce5224974b4a86029a9eb9b350d2be305
gianthead97 <nikola_jankovic@tuta.io>
2020-07-01 08:36:05 +0200

    popravio

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
-// let distDir = __dirname + "/dist/";
-// app.use(express.static(distDir));
+let distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 app.use("/", routes);
 
