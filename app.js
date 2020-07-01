51f3b4497ae01370e42ad781ec23c66844c09b90
gianthead97 <nikola_jankovic@tuta.io>
2020-07-01 06:53:15 +0200

    morgan comm

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./api/routes/routes');
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
