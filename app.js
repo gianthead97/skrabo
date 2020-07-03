8f975a8aaf76aa3e0dc4e3d77261a2b90de3af22
gianthead97 <nikola_jankovic@tuta.io>
2020-07-03 03:10:17 +0200

    test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-
 //const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./api/routes/routes');
 
 // app.use(morgan('dev'));
