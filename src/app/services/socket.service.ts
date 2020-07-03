4209c022de30f89bb80610b74e161edf740cc89f
gianthead97 <nikola_jankovic@tuta.io>
2020-07-03 04:08:00 +0200

    test

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -7,18 +7,10 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class SocketService {
-  private URL: string = 'http://localhost:3000';
+  private URL: string = 'https://skrabo.herokuapp.com:3000';
   private sock = io(this.URL);
   constructor(private http: HttpClient) {
-    // this.http.get<{url}>('http://localhost:3000' + '/socketPort', {
-    //   withCredentials: false
-    // }).subscribe(response => {
-    //   this.URL += response.url;
-    //   console.log(this.URL);
-    //   this.sock = io(this.URL);
-      
-    // });
-  }
+ }
 
   get socket() {
     return this.sock;
