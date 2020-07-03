478a15124f49211eccc7b7095d3da2884f659e63
gianthead97 <nikola_jankovic@tuta.io>
2020-07-03 02:55:14 +0200

    test

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const app = express();
+
 //const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const routes = require('./api/routes/routes');
 
 // app.use(morgan('dev'));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
- // Add this
- if (req.method === 'OPTIONS') {
-
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
-      res.header('Access-Control-Max-Age', 120);
-      return res.status(200).json({});
-  }
-
-  next();
-
-});
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const socketServer = require('./api/service/socketServer');
 new socketServer(server).start();
 
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server listen on port ${port}.`);
 });
