502d463502a132dcf91c6f00a2026a3fa6a6734e
gianthead97 <nikola_jankovic@tuta.io>
2020-07-20 23:31:57 +0200

    add some logic on server side

--- api/models/player.js
+++ api/models/player.js
@@ -1,4 +1,19 @@
+module.exports = class Player {
+    #_points = 0;
+    constructor(name) {
+        this.#name = name;
+    }
 
-class Player {
-    constructor(name)
+
+    get points() {
+        return this.#_points;
+    }
+
+    increasePoints(newPoints) {
+        this.#_points += newPoints;
+    }
+
+
+
+
 }
--- api/models/room.js
+++ api/models/room.js
@@ -1,11 +1,25 @@
 module.exports = class Room {
     #players = [];
+    #closed = false;
+    #numOfRounds;
     constructor(roomId, roomName) {
         this.id = roomId;
         this.roomName = roomName;
     }
     get roomId() {
         return this.id;
+    }
+
+    set numberOfRounds(num) {
+        this.#numOfRounds = num;
+    }
+
+    get isClosed() {
+        return this.#closed;
+    }
+
+    joinNewPlayer(player) {
+        this.#players.push(player);
     }
     
 }
